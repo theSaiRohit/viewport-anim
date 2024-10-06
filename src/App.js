4b47ff44b1b35537ed05186b9f485c5bbd8674d5
Sai Rohit <sairohit11.s@gmail.com>
2024-10-06 14:14:53 +0530

    Refactor CSS styles and add mouse interaction functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [mouseLeave, setMouseLeave] = useState(false);
+
+  const containerRef = useRef(null);
+  const winHeight = window.innerHeight / 2;
+  const winWidth = window.innerWidth / 2;
+
+  function onMouseLeave() {
+    document.addEventListener("mouseleave", () => {
+      setPosition({ x: 0, y: 0 });
+      setMouseLeave(true);
+    });
+  }
+
+  function mouseMove(e) {
+    setPosition({ x: winWidth - e.clientX, y: winHeight - e.clientY });
+    setMouseLeave(false);
+    return onMouseLeave();
+  }
+
+  const limiterNum = 3.5;
+
+  const oneStyle = {
+    left: -position.x / limiterNum + "px",
+    top: -position.y / limiterNum + "px",
+    transition: mouseLeave ? "all 0.5s ease" : "none"
+  };
+  const twoStyle = {
+    left: +position.x / limiterNum + "px",
+    top: -position.y / limiterNum + "px",
+    transition: mouseLeave ? "all 0.5s ease" : "none"
+  };
+  const threeStyle = {
+    left: -position.x / limiterNum + "px",
+    top: +position.y / limiterNum + "px",
+    transition: mouseLeave ? "all 0.5s ease" : "none"
+  };
+  const fourStyle = {
+    left: position.x / limiterNum + "px",
+    top: position.y / limiterNum + "px",
+    transition: mouseLeave ? "all 0.5s ease" : "none"
+  };
+  console.log("this", position.x, position.y);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" onMouseMove={mouseMove} ref={containerRef}>
+      <div className="circles"></div>
+      <div className="circles" style={oneStyle}></div>
+      <div className="circles" style={twoStyle}></div>
+      <div className="circles" style={threeStyle}></div>
+      <div className="circles" style={fourStyle}></div>
     </div>
   );
 }
---
+++ src/mouse.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function mouse() {
+  return <div>mouse</div>;
+}
